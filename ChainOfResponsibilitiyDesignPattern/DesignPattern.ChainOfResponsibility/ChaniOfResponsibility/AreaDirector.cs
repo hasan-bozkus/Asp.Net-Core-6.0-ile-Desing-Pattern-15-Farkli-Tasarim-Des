1a5a131f35fa7e6ea6b30637c0408524579c2601
Hasan BOZKUŞ <103758391+hasan-bozkus@users.noreply.github.com>
2024-09-10 16:28:11 +0300

    Asp.Net Core 6.0 ile Desing Pattern (15 Farklı Tasarım Des)
    Bölüm 1 - Chain Of Responsibility Design Pattern (Asp.Net Core 5.0 - MSSQL)
    
    Giriş,
    Teorik Anlatım,
    Paketlerin Kurulması,
    Context ve Entity Sınıfları,
    Migration İşlemi,
    Employee İşlemi,
    Treasurer İşlemi,
    Manager Assistant Sınfı,
    Manager Sınıfı,
    Manager Sınıfı,
    Area Director Sınıfı,
    Controller,
    Uygulamanın Test Edilmesi

---
+++ ChainOfResponsibilitiyDesignPattern/DesignPattern.ChainOfResponsibility/ChaniOfResponsibility/AreaDirector.cs
@@ -0,0 +1,35 @@
+﻿using DesignPattern.ChainOfResponsibility.DAL;
+using DesignPattern.ChainOfResponsibility.Models;
+
+namespace DesignPattern.ChainOfResponsibility.ChaniOfResponsibility
+{
+    public class AreaDirector : Employee
+    {
+        public override void PocessRequest(CustomerProcessViewModel req)
+        {
+            Context context = new Context();
+            if (req.Amout <= 400000)
+            {
+                CustomerProcess customerProcess = new CustomerProcess();
+                customerProcess.Amout = req.Amout.ToString();
+                customerProcess.Name = req.Name;
+                customerProcess.EmployeeName = "Bölge Direktörü - Zeynep Yılmaz";
+                customerProcess.Description = "Para Çekme İşlei Onaylandı, Müşteriye Talep Ettiği Tutar Ödendi";
+                context.CustomerProcesses.Add(customerProcess);
+                context.SaveChanges();
+            }
+            else
+            {
+                CustomerProcess customerProcess = new CustomerProcess();
+                customerProcess.Amout = req.Amout.ToString();
+                customerProcess.Name = req.Name;
+                customerProcess.EmployeeName = "Bölge Direktörü - Zeynep Yılmaz";
+                customerProcess.Description = "Para Çekme Tutarı Bölge Direktörünün Günlük Ödeyebileceği Limiti Aştığı İçin İşlem Gerçekleştirilemedi, Müşterinin Günlük Maksimum Çekebileceği Tutar 400.000₺ olup daha fazlası için birden fazla gün şubeye gelmesi gerekli...";
+                context.CustomerProcesses.Add(customerProcess);
+                context.SaveChanges();
+                
+            }
+        }
+
+    }
+}
---
+++ ChainOfResponsibilitiyDesignPattern/DesignPattern.ChainOfResponsibility/Controllers/DefaultController.cs
@@ -0,0 +1,33 @@
+﻿using DesignPattern.ChainOfResponsibility.ChaniOfResponsibility;
+using DesignPattern.ChainOfResponsibility.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DesignPattern.ChainOfResponsibility.Controllers
+{
+    public class DefaultController : Controller
+    {
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(CustomerProcessViewModel customerProcessViewModel)
+        {
+            Employee truasurer = new Treasurer();
+            Employee managerAssistant = new ManageAssistant();
+            Employee manager = new Manager();
+            Employee areaDirector = new AreaDirector();
+
+            truasurer.SetNextApprover(managerAssistant);
+            managerAssistant.SetNextApprover(manager);
+            manager.SetNextApprover(areaDirector);
+
+            truasurer.PocessRequest(customerProcessViewModel);
+
+            return RedirectToAction("Index");
+        }
+    }
+}
