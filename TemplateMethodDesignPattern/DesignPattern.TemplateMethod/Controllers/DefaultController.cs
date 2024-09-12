7edb3f3a418d6456381754d74a64aae3c505d21a
Hasan BOZKUŞ <103758391+hasan-bozkus@users.noreply.github.com>
2024-09-12 10:45:40 +0300

    Asp.Net Core 6.0 ile Desing Pattern (15 Farklı Tasarım Des)
    Bölüm 3 - Template Method Design Pattern (Asp.Net Core 5.0)
    Giriş Netflix Plan Örneği,
    Override,
    Netflix Plans Abstract Class,
    Basic Plan,
    Ultra Plan

---
+++ TemplateMethodDesignPattern/DesignPattern.TemplateMethod/Controllers/DefaultController.cs
@@ -0,0 +1,45 @@
+﻿using DesignPattern.TemplateMethod.TemplatePattern;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DesignPattern.TemplateMethod.Controllers
+{
+    public class DefaultController : Controller
+    {
+        public IActionResult BasicPlanIndex()
+        {
+            NetflixPlans netflixPlans = new BasicPlan();
+            //ViewBag.planType = netflixPlans.PlanType("Temel Plan");
+            //ViewBag.countPerson = netflixPlans.CountPerson(1);
+            //ViewBag.price = netflixPlans.Price(65.99);
+            //ViewBag.content = netflixPlans.Content("Film-Dizi");
+            //ViewBag.resolution = netflixPlans.Resolution("480px");
+            netflixPlans.CreatePlan();
+
+            return View(netflixPlans);
+        }
+
+        public IActionResult StandardPlanIndex()
+        {
+            NetflixPlans netflixPlans = new StandardPlan();
+            ViewBag.planType = netflixPlans.PlanType("Standart Plan");
+            ViewBag.countPerson = netflixPlans.CountPerson(2);
+            ViewBag.price = netflixPlans.Price(94.99);
+            ViewBag.content = netflixPlans.Content("Film-Dizi-Animasyon");
+            ViewBag.resolution = netflixPlans.Resolution("720px");
+
+            return View(netflixPlans);
+        }
+
+        public IActionResult UltraPlanIndex()
+        {
+            NetflixPlans netflixPlans = new UltraPlan();
+            ViewBag.planType = netflixPlans.PlanType("Ultra Plan");
+            ViewBag.countPerson = netflixPlans.CountPerson(4);
+            ViewBag.price = netflixPlans.Price(134.99);
+            ViewBag.content = netflixPlans.Content("Film-Dizi-Animasyon-Belgesel");
+            ViewBag.resolution = netflixPlans.Resolution("1080px");
+
+            return View(netflixPlans);
+        }
+    }
+}
