dfab79490f23af5c386942b8efc08b729f1f712c
Hasan BOZKUŞ <103758391+hasan-bozkus@users.noreply.github.com>
2024-09-20 15:21:32 +0300

    Asp.Net Core 6.0 ile Desing Pattern (15 Farklı Tasarım Des)
    Bölüm 5 - Unit Of Work Design Pattern (Asp.Net Core 5.0 - PostgreSQL)
    Giriş,
    Projenin Oluşturulması,
    Paketlerin Kurulması ve Entity Katmanı,
    Referans İşlemleri ve PostgreSQL Connection String,
    Migration İşlemi,
    Interfacelerin Tanımlanması,
    Generic Repository ve EfDal Sınıfları,
    Manager ve Servicelerin Yazılması,
    Controllerın Yazılması,
    Örnek Veri Girişi ve View,
    Uygulamanın Test Edilmesi

---
+++ UnitOfWorkDesignPattern/DesignPattern.BusinessLayer/Concrete/ProcessManager.cs
@@ -0,0 +1,55 @@
+﻿using DesignPattern.BusinessLayer.Abstract;
+using DesignPattern.DataAccessLayer.Abstract;
+using DesignPattern.DataAccessLayer.UnitOfWork;
+using DesignPattern.EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPattern.BusinessLayer.Concrete
+{
+    public class ProcessManager : IProcessService
+    {
+
+        private readonly IProcessDal _processDal;
+        private readonly IUowDal _uowDal;
+
+        public ProcessManager(IProcessDal processDal, IUowDal uowDal)
+        {
+            _processDal = processDal;
+            _uowDal = uowDal;
+        }
+
+        public void TDelete(Process t)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Process TGetByID(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Process> TGetList()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void TInsert(Process t)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void TMultiUpdate(List<Process> t)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void TUpdate(Process t)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
