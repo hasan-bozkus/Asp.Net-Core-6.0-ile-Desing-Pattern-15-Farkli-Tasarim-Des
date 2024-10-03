e127e1d8e61d62a451a76a1e29fdcb9325721c7c
Hasan BOZKUŞ <103758391+hasan-bozkus@users.noreply.github.com>
2024-10-03 14:09:40 +0300

    Asp.Net Core 6.0 ile Desing Pattern (15 Farklı Tasarım Des)
    Bölüm 8 - IteratorDesign Pattern (Asp.Net Core 6.0 - MSSQL)
    Giriş,
    Gerçek Dünya Örneği,
    Visit Route - Iterator - IMover,
    VisitRouterMover Sınıfı,
    Visit Route Iterator Sınıfı,
    Controller Kısmı ve Uygulamanın Testi

---
+++ IteratorDesignPattern/DesignPattern.Iterator/IteratorPattern/VisitRouteMover.cs
@@ -0,0 +1,19 @@
+﻿namespace DesignPattern.Iterator.IteratorPattern
+{
+    public class VisitRouteMover : IMover<VisitRoute>
+    {
+        public List<VisitRoute> VisitRoutes = new List<VisitRoute>();
+
+        public void AddVisitRoute(VisitRoute visitRoute)
+        {
+            VisitRoutes.Add(visitRoute);
+        }
+
+        public int VisitRouteCount { get => VisitRoutes.Count; }
+
+        public IIterator<VisitRoute> CreateIterator()
+        {
+            return new VisitRouteIterator(this);
+        }
+    }
+}
