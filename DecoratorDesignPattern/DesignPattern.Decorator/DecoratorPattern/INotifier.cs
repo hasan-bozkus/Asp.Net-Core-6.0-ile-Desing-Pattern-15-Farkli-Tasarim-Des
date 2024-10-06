1b2ce55573f36ca06b4f6b4143d0d777caab08ff
Hasan BOZKUŞ <103758391+hasan-bozkus@users.noreply.github.com>
2024-10-06 10:58:47 +0300

    Asp.Net Core 6.0 ile Desing Pattern (15 Farklı Tasarım Des)
    Bölüm 11 - Decorator Design Pattern (Asp.Net Core 5.0 - MSSQL)
    Giriş,
    Şema,
    Kahve Örneği,
    Bağlantı sınıfı ve Entity,
    Notifier Sınıfı,
    INotifier ve CreateNotifier,
    Decorator Sınıfı,
    SendMail Decorator,
    Mesaj Sınıfı Örneği,
    Encrypto By Subject Decorator,
    Encrypto By Content Decorator,
    Uygulamanın Test Edilmesi,
    Son Test

---
+++ DecoratorDesignPattern/DesignPattern.Decorator/DecoratorPattern/INotifier.cs
@@ -0,0 +1,10 @@
+﻿using DesignPattern.Decorator.DAL;
+
+namespace DesignPattern.Decorator.DecoratorPattern
+{
+    public interface INotifier
+    {
+        void CreateNotify(Notifier notifier);
+
+    }
+}
